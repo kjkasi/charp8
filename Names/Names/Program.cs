ef32a4b8803e8c8e85320b832d09873a38ce0eac
kjkasi <root@localhost.localdomain>
2022-04-03 15:22:47 +0300

    lab Names

---
+++ Names/Names/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Имя: ");
+                string name = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    names.Add(name);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            names.Sort();
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
