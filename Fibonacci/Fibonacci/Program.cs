0282a2ed34057b06ab18eabde771a13c723991dc
kjkasi <root@localhost.localdomain>
2022-04-03 12:27:36 +0300

    lab 2

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k1 = 1;
+            int k2 = 1;
+
+            int k3;
+
+            Console.WriteLine(k1);
+            Console.WriteLine(k2);
+
+            while ((k3 = k1 + k2) < 1000)
+            {
+                Console.WriteLine(k3);
+                k1 = k2;
+                k2 = k3;
+            }
+        }
+    }
+}
